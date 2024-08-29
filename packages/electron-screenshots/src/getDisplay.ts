b239c9d74598da7c43282a5472a16b4669d479b0
邓学儒 <dengxueru@hengli.com>
2024-08-29 11:28:16 +0800

    feat: windows 下放大的情况下无法截图问题

--- packages/electron-screenshots/src/getDisplay.ts
+++ packages/electron-screenshots/src/getDisplay.ts
@@ -8,14 +8,12 @@ export interface Display extends Rectangle {
 export default (): Display => {
   const point = screen.getCursorScreenPoint()
   const { id, bounds, scaleFactor } = screen.getDisplayNearestPoint(point)
-
-  // https://github.com/nashaofu/screenshots/issues/98
   return {
     id,
     scaleFactor,
-    x: Math.floor(bounds.x * scaleFactor),
-    y: Math.floor(bounds.y * scaleFactor),
-    width: Math.floor(bounds.width * scaleFactor),
-    height: Math.floor(bounds.height * scaleFactor)
+    x: bounds.x,
+    y: bounds.y,
+    width: bounds.width,
+    height: bounds.height
   }
 }
--- packages/electron-screenshots/src/screenshots.ts
+++ packages/electron-screenshots/src/screenshots.ts
@@ -109,7 +109,6 @@ export default class Screenshots extends Events {
 
   updateBounds () {
     const display = getDisplay()
-    console.log(display.id)
     if (this.screenId !== display.id) {
       this.endCapture()
       this.startCapture()
@@ -288,8 +287,8 @@ export default class Screenshots extends Events {
     // this.$win.setFullScreen(isMac)
     // this.$win.setSimpleFullScreen(isMac)
     const { width, scaleFactor, height } = display // 截图的时候按照DPI截图，这里要按照DPI还原成原始大小
-    display.width = width / scaleFactor
-    display.height = height / scaleFactor
+    display.width = width
+    display.height = height
     this.$win.setBounds(display, false)
     // this.$view.setBounds({
     //   x: 0,
